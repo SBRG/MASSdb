656c6120741101a3a9f8b048c3705500c7e29e96
z-haiman <zhaiman@eng.ucsd.edu>
2021-05-02 00:35:04 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""This module contains the setup for the installation of the MASS-project."""
+
+# All arguments are defined in `setup.cfg`.
+import setuptools
+
+setuptools.setup()
